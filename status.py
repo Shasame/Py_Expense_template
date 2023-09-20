62ea320634593eab07edc112ba8d3ea4c2818c45
Hadrien <hcombaz@gmail.com>
2023-09-20 21:57:46 +0200

    WIP status

--- status.py
+++ status.py
@@ -5,12 +5,18 @@ def get_status():
     status = {}
     expenses = get_expenses()
     users = get_user()
-    for user in users:
-        status[user] = 0
     for expense in expenses:
-        amount, label, spender, owner = expense.split(",")
-        status[spender] += float(amount)
-    print("Status:")
+        spenderInfo, people = expense.split("|")
+        amount, label, spender = spenderInfo.split(",")
+        #for every expense calculate the amount of money all the other in people owe to spender
+        if spender not in status:
+            status[spender] = 0
+        for person in people.split(","):
+            if person not in status:
+                status[person] = 0
+            status[person] -= float(amount)/len(people.split(","))
+        status[spender] += float(amount) - float(amount)/len(people.split(","))
+    print("Users:")
     for user in status:
         print(f"{user} : {status[user]}")
     return
